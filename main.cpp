be430df2a5213d635c978d2418e059a1c1ec0e4d
jserres <eleve@stationLinux.iris>
2016-01-28 17:28:45 +0100

    Test avec netbean

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 28 janvier 2016, 17:21
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "bonjour";
+    return 0;
+}
+
